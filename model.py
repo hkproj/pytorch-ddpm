b326dfb0b71e2fc4904d47dda4e858369dc0aa56
Umar Jamil <hkproj@gmail.com>
2023-07-02 15:11:57 +0800

    added comments on the two algorithms from the paper

--- model.py
+++ model.py
@@ -19,22 +19,24 @@ class DiffusionModel(pl.LightningModule):
         return self.unet(x, t)
 
     def beta(self, t):
-        return self.beta_small + (t / self.t_range) * (
-            self.beta_large - self.beta_small
-        )
+        # Just a simple linear interpolation between beta_small and beta_large based on t
+        return self.beta_small + (t / self.t_range) * (self.beta_large - self.beta_small)
 
     def alpha(self, t):
         return 1 - self.beta(t)
 
     def alpha_bar(self, t):
+        # Product of alphas from 0 to t
         return math.prod([self.alpha(j) for j in range(t)])
 
     def get_loss(self, batch, batch_idx):
         """
         Corresponds to Algorithm 1 from (Ho et al., 2020).
         """
+        # Get a random time step for each image in the batch
         ts = torch.randint(0, self.t_range, [batch.shape[0]], device=self.device)
         noise_imgs = []
+        # Generate noise, one for each image in the batch
         epsilons = torch.randn(batch.shape, device=self.device)
         for i in range(len(ts)):
             a_hat = self.alpha_bar(ts[i])
@@ -42,7 +44,9 @@ class DiffusionModel(pl.LightningModule):
                 (math.sqrt(a_hat) * batch[i]) + (math.sqrt(1 - a_hat) * epsilons[i])
             )
         noise_imgs = torch.stack(noise_imgs, dim=0)
+        # Run the noisy images through the U-Net, to get the predicted noise
         e_hat = self.forward(noise_imgs, ts)
+        # Calculate the loss, that is, the MSE between the predicted noise and the actual noise
         loss = nn.functional.mse_loss(
             e_hat.reshape(-1, self.in_size), epsilons.reshape(-1, self.in_size)
         )
@@ -57,7 +61,9 @@ class DiffusionModel(pl.LightningModule):
                 z = torch.randn(x.shape)
             else:
                 z = 0
+            # Get the predicted noise from the U-Net
             e_hat = self.forward(x, t.view(1).repeat(x.shape[0]))
+            # Perform the denoising step to take the image from t to t-1
             pre_scale = 1 / math.sqrt(self.alpha(t))
             e_scale = (1 - self.alpha(t)) / math.sqrt(1 - self.alpha_bar(t))
             post_sigma = math.sqrt(self.beta(t)) * z
